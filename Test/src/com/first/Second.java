b8e11dbd99bdfb9aa2791169c90b4e3076adcc10
YaladagiSeema <seema.yaladagi156@gmail.com>
2018-01-09 13:02:09 +0530

    second class modification

--- Test/src/com/first/Second.java
+++ Test/src/com/first/Second.java
@@ -8,4 +8,11 @@ public class Second {
 		int sum =a+b;
 		System.out.println("sum result is " +sum);
 	}
+
+	public void sub(int a,int b)
+	{
+		int sub =a-b;
+		System.out.println("sum result is " +sub);
+	}
+
 }
